399ea6bf3097b95ba36e38054a689867be67767b
Dowen <tokenyete@gmail.com>
2022-04-08 13:57:20 +0800

    fix: :pencil2: typo fix

--- lib/pages/app_page/pages/server_page/machine/states/jar_dangerous_ask_state.dart
+++ lib/pages/app_page/pages/server_page/machine/states/jar_dangerous_ask_state.dart
@@ -1,7 +1,7 @@
 import 'package:minecraft_cube_desktop/_utilities/console_line_util.dart';
 import 'package:minecraft_cube_desktop/pages/app_page/pages/server_page/machine/server_machine.dart';
-import 'package:minecraft_cube_desktop/pages/app_page/pages/server_page/machine/states/eula_ask_state.i18n.dart';
 import 'package:minecraft_cube_desktop/pages/app_page/pages/server_page/machine/states/istate.dart';
+import 'package:minecraft_cube_desktop/pages/app_page/pages/server_page/machine/states/jar_dangerous_ask_state.i18n.dart';
 
 class JarDangerousAskState extends IState {
   JarDangerousAskState(
@@ -12,7 +12,7 @@ class JarDangerousAskState extends IState {
   Future<void> agree() async {
     machine.addLog(
       generateOneLineConsoleLine(
-        eulaAskAgreeFailure.i18n,
+        jarDangerousAskAgree.i18n,
       ),
     );
     machine.state = machine.configurationLoaderState;
@@ -23,7 +23,7 @@ class JarDangerousAskState extends IState {
   void disagree() {
     machine.addLog(
       generateOneLineConsoleLine(
-        eulaAskDisagree.i18n,
+        jarDangerousAskDisagree.i18n,
       ),
     );
     machine.state = machine.idleState;
